2c6c0c886813a2ef5760978b2281ba26651feac4
wassim snen <wassimsne@yahoo.fr>
2022-02-07 22:13:02 +0100

    message

---
+++ src/test/java/Test/FirstUnitTest.java
@@ -0,0 +1,18 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+//import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
+
+public class FirstUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
